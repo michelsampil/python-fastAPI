6570035fa67aa2052972f3d83674d42a0e401597
michel sampil <michel.sampil@gmail.com>
2024-08-04 23:55:43 -0300

    WIP building basic apps

---
+++ fastapi/books.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'id': '1','title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'id': '2','title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'id': '3','title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'id': '4','title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'id': '5','title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'id': '6','title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/{id}")
+async def read_book(id):
+    for book in BOOKS:
+        if book.get('id').casefold() == id.casefold():
+            return book
+        else:
+            return 'book not found'
